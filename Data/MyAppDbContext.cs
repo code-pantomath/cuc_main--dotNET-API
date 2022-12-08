b7870cb31bfcc1e1b9d40c9301e8eaafae483224
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-08 23:14:51 +0300

    Update MyAppDbContext.cs

--- Data/MyAppDbContext.cs
+++ Data/MyAppDbContext.cs
@@ -9,7 +9,7 @@ namespace CheapUdemy.Data
         public MyAppDbContext(DbContextOptions options) : base(options)
         {
 
-            
+            ///...
 
         }
 
@@ -25,74 +25,65 @@ namespace CheapUdemy.Data
 
 
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
+//         protected override void OnModelCreating(ModelBuilder modelBuilder)
+//         {
+//            base.OnModelCreating(modelBuilder);
 
 
-        //    //modelBuilder.Entity<Wallet>().Property(w => w.History).HasColumnName("Id");
+//            modelBuilder.Entity<Wallet>().Property(w => w.History).HasColumnName("Id")
+//            modelBuilder.Entity<User>().OwnsOne(u => u.Wallet).OwnsOne(uw => uw.History);
+
+//            modelBuilder.Entity<User>().OwnsOne(u => u.Udem).OwnsOne(ud => ud.UdemyAccounts);
+
+//            modelBuilder.Entity<User>().Property(u => u.UdemAccounts)
+//               .HasConversion(
+//                   v => JsonSerializer.Serialize(v, null),
+//                   v => JsonSerializer.Deserialize<List<UdemyAccount>>(v, null));
+
+//            modelBuilder.Entity<User>().Property(u => u.UdemAccounts);
+
+//            modelBuilder.Entity<UdemyAccount>().Navigation(u => u.Name).IsRequired();
+//            modelBuilder.Entity<UdemyAccount>().Navigation(u => u.Email).IsRequired();
+
+//            modelBuilder.Entity<User>()
+//               .HasOne(u => u.UserWallet)
+//               .WithOne(uw => uw.User)
+//               .HasForeignKey<UserWallet>(uw => uw.WalletId)
+//            ;
+
+//         }
 
 
-        //    /////
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
+           base.OnModelCreating(modelBuilder);
+           modelBuilder.Entity<User>().HasData(new User[] // This methods receives an array of the objects you want to save as default objects that exists before any other ones...
+           {
 
-        //    //modelBuilder.Entity<User>().OwnsOne(u => u.Wallet).OwnsOne(uw => uw.History);
+               new User
+               {
+                   UserId = 0,
+                   FirstName = "admin1",
+                   LastName = "_",
+                   Email = "admin1@THE_WEBSITE_DOMAIN_NAME.com",
+                   Password = "THE_ADMIN1_PSWD",
+                   IP = null,
+               },
 
-        //    //modelBuilder.Entity<User>().OwnsOne(u => u.Udem).OwnsOne(ud => ud.UdemyAccounts);
+               new User
+               {
+                   UserId = 1,
+                   FirstName = "webmaster1",
+                   LastName = "_",
+                   Email = "webmaster1@THE_WEBSITE_DOMAIN_NAME.com",
+                   Password = "THE_WEBMASTER1_PSWD",
+                   IP = null,
+               },
 
-        //    //modelBuilder.Entity<User>().Property(u => u.UdemAccounts)
-        //    //    .HasConversion(
-        //    //        v => JsonSerializer.Serialize(v, null),
-        //    //        v => JsonSerializer.Deserialize<List<UdemyAccount>>(v, null));
-
-        //    //modelBuilder.Entity<User>().Property(u => u.UdemAccounts);
-
-        //    //modelBuilder.Entity<UdemyAccount>().Navigation(u => u.Name).IsRequired();
-        //    //modelBuilder.Entity<UdemyAccount>().Navigation(u => u.Email).IsRequired();
-
-
-
-        //    //modelBuilder.Entity<User>()
-        //    //    .HasOne(u => u.UserWallet)
-        //    //    .WithOne(uw => uw.User)
-        //    //    .HasForeignKey<UserWallet>(uw => uw.WalletId)
-        //    //;
-
-
-        //}
-
-
-        // This is how to create default data when the database-server first runs...
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-
-        //    base.OnModelCreating(modelBuilder);
-        //    modelBuilder.Entity<User>().HasData(new User[] // This methods receives an array of the objects you want to save as default objects that exists before any other ones...
-        //    {
-
-        //        new User
-        //        {
-        //            UserId = 1,
-        //            FirstName = "Rashad",
-        //            LastName = "Rashad",
-        //            Email = "fedevalverde777777777@gmail.com",
-        //            Password = "this is a pswd",
-        //            IP = null,
-        //        },
-
-        //        new User
-        //        {
-        //            UserId = 2,
-        //            FirstName = "Mo",
-        //            LastName = "Ad",
-        //            Email = "tortorfatal123321@gmail.com",
-        //            Password = "this is a pswd",
-        //            IP = null,
-        //        }
-
-        //    });
-        //}
-        ///
+           });
+        }
+        /
 
 
     }
