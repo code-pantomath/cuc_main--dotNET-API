c09e316406e344169215a95763fd67c43567f51f
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-08 23:07:37 +0300

    Update CONSTANTS.cs

--- CONSTANTS.cs
+++ CONSTANTS.cs
@@ -5,12 +5,8 @@
 
         public static byte SERVICE_ACCOUNT_PRICE { get; } = 6;
         public static byte SERVICE_GIFT_PRICE { get; } = 7;
-        /////
-        ///
 
         public static byte[] WALLET_CHARGE_RANGES { get; } = new byte[5] { 6, 7, 12, 14, 28 };
-        ///
-
 
         //public static Dictionary<byte, int> PAYMENTGATEWAYSIDS { get; } = new Dictionary<byte, int>()
         //{
